9d328037097b3093b06d0719000bca986b7753c3
Joshua Gibson <flash50k@gmail.com>
2019-08-17 14:57:44 +0100

    vuetify running successfully

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,31 +1,36 @@
 <template>
-  <div id="app">
-    <p v-for="(launch, index) in launches">Name: {{launch.name}}</p>
-  </div>
+  <v-app>
+    <v-app-bar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Vuetify</span>
+        <span class="font-weight-light">MATERIAL DESIGN</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+        text
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+      >
+        <span class="mr-2">Latest Release</span>
+      </v-btn>
+    </v-app-bar>
+
+    <v-content>
+      <HelloWorld/>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import APIservice from '@/services/APIService.js'
+import HelloWorld from './components/HelloWorld';
 
 export default {
-  name: 'app',
-  data() {
-    return {
-      launches: []
-    }
+  name: 'App',
+  components: {
+    HelloWorld,
   },
-  mounted() {
-    this.fetchData()
-  },
-  methods: {
-    fetchData(){
-      APIservice.getLaunches()
-      .then(launches => this.launches = launches.launches);
-    }
-  }
-}
+  data: () => ({
+    //
+  }),
+};
 </script>
-
-<style>
-
-</style>
