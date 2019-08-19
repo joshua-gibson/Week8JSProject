d846df01426a64faa4f8e76ce39e66507bd033bd
Joshua Gibson <flash50k@gmail.com>
2019-08-19 16:46:21 +0100

    refactored slightly, now working

--- server/db/bulkSubmit.js
+++ server/db/bulkSubmit.js
@@ -1,5 +1,4 @@
 // To use this, first install via npm: npm i node-fetch --save
-// Then: npm install mongoose --save
 //  Then: node .\db\bulkSubmit.js
 // import APIservice from '@/services/APIService.js'
 
@@ -9,36 +8,30 @@ const fetch = require("node-fetch");
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/";
 
-MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  var dbo = db.db("mydb");
+
 
 let cohort = [];
-let getCohort=function(num){fetch('https://launchlibrary.net/1.4/launch?sort=%27ascending%27&offset=${num}0')
+let getCohort=function(num){fetch(`https://launchlibrary.net/1.4/launch?sort=%27ascending%27&offset=${num}0`)
   .then(res=>res.json())
   .then(res=> cohort = res)
   .then(cohort=> {
 
-    dbo.collection("launches").insertMany(cohort.launches, function(err, res) {
+    MongoClient.connect(url, function(err, db) {
       if (err) throw err;
-      console.log("Number of documents inserted: " + res.insertedCount);
-      db.close();
-    });
-
-  }
-
-  )
-  };
-//
-// getCohort(1);
-// getCohort(2);
-// getCohort(3);
+      var dbo = db.db("mydb");
+      dbo.collection("launches").insertMany(cohort.launches, function(err, res) {
+        if (err) throw err;
+        console.log("Number of documents inserted: " + res.insertedCount);
+        db.close();
+      })
+    })
 
 
-let i;
-for (i = 0; i <= 169; i++) {
+
+  })
+};
+
+let i=0;
+for (i = 0; i < 170; i++) {
   getCohort(i);
 }
-
-
-});
