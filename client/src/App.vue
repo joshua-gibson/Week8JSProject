f3150c5fb482861611ffb621d781f6fe666f1abf
lawrie-sm <alertrelic@gmail.com>
2019-08-19 11:17:05 +0100

    Made splash component, scaled with window height

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <h1>Launches!</h1>
+    <launch-splash />
     <launch-timeline :launches="launches" />
   </div>
 </template>
@@ -8,9 +8,14 @@
 <script>
 import APIservice from '@/services/APIService.js'
 import LaunchTimeline from '@/components/LaunchTimeline.vue'
+import LaunchSplash from '@/components/LaunchSplash.vue'
+
 export default {
   name: 'app',
-  components: { 'launch-timeline': LaunchTimeline },
+  components: {
+    'launch-timeline': LaunchTimeline,
+    'launch-splash': LaunchSplash
+  },
   data() {
     return {
       launches: []
---
+++ client/src/components/LaunchSplash.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="splash" :style="{ height: splashHeight }">
+    <h1>Launches!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LaunchSplash',
+  data() {
+    return { splashHeight: '1000px' }
+  },
+  mounted() {
+    this.splashHeight = `${window.innerHeight}px`;
+  }
+}
+</script>
+
+<style>
+#splash {
+}
+</style>
