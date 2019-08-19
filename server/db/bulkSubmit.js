a0c88f4a33bc8767068f3b2005ec3c8db197fee2
Joshua Gibson <flash50k@gmail.com>
2019-08-19 16:12:29 +0100

    will populate a mongo DB. 169 separate queries

--- server/db/bulkSubmit.js
+++ server/db/bulkSubmit.js
@@ -2,6 +2,9 @@
 // Then: npm install mongoose --save
 //  Then: node .\db\bulkSubmit.js
 // import APIservice from '@/services/APIService.js'
+
+//1690 is the last page
+
 const fetch = require("node-fetch");
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/";
@@ -11,22 +14,31 @@ MongoClient.connect(url, function(err, db) {
   var dbo = db.db("mydb");
 
 let cohort = [];
-let x=function(){fetch('https://launchlibrary.net/1.4/launch?sort=%27ascending%27&offset=0')
+let getCohort=function(num){fetch('https://launchlibrary.net/1.4/launch?sort=%27ascending%27&offset=${num}0')
   .then(res=>res.json())
   .then(res=> cohort = res)
   .then(cohort=> {
-    // cohort.launches.forEach(function(cVal, index){console.log(cVal)})
-    dbo.collection("launches").insertMany(cohort, function(err, res) {
+
+    dbo.collection("launches").insertMany(cohort.launches, function(err, res) {
       if (err) throw err;
       console.log("Number of documents inserted: " + res.insertedCount);
       db.close();
     });
+
   }
 
   )
   };
+//
+// getCohort(1);
+// getCohort(2);
+// getCohort(3);
 
-x();
+
+let i;
+for (i = 0; i <= 169; i++) {
+  getCohort(i);
+}
 
 
 });
