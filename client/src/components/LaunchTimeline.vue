8fac210b7b028f83a41d7ba153ab706914fae918
lawrie-sm <alertrelic@gmail.com>
2019-08-19 15:41:17 +0100

    Moved fetch data into timeline

--- client/src/components/LaunchTimeline.vue
+++ client/src/components/LaunchTimeline.vue
@@ -9,38 +9,32 @@
 </template>
 
 <script>
+import APIservice from '@/services/APIService.js';
 import LaunchCard from '@/components/LaunchCard.vue';
 export default {
   name: 'LaunchTimeline',
   components: { 'launch-card': LaunchCard },
-  props: ['launches'],
+  data() {
+    return {
+      launches: []
+    }
+  },
+  mounted() {
+    this.fetchData()
+  },
+  methods: {
+    fetchData(){
+      APIservice.getLaunches()
+      .then(launches => this.launches = launches.launches);
+    }
+  }
 }
 </script>
 
 <style lang="css" scoped>
-/*
-.TimelineContainer {
-display: flex;
-flex-grow: 2;
-margin: 50px;
-border: 5px;
-padding: 50px;
-width: 1000px;
-height: 1000px;
-min-width: 500px;
-max-width: 1500px;
-border: solid grey 1px;
-overflow: auto;
--webkit-box-shadow: 3px 3px 20px 0px rgba(130,128,130,0.48);
--moz-box-shadow: 3px 3px 20px 0px rgba(130,128,130,0.48);
-box-shadow: 3px 3px 20px 0px rgba(130,128,130,0.48);
-}
- */
-
 .buffer {
   display: flex;
 }
-
 
 .tl {
   font-family: 'Josefin Sans', sans-serif;
