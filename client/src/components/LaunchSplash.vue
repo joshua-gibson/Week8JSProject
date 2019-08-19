213e53d8d4edaaa5bce2b0d8dd61ef0eae312929
andyymckay <andymckay1992@hotmail.co.uk>
2019-08-19 11:56:48 +0100

    added background image to splash comp

--- client/src/components/LaunchSplash.vue
+++ client/src/components/LaunchSplash.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="splash" :style="{ height: splashHeight }">
     <h1>Launches!</h1>
+    <!-- <video autoplay loop id="video-background" muted plays-inline>
+  <source src="https://www.youtube.com/watch?v=wnhvanMdx4s" type="video/mp4">
+</video> -->
+<img src="https://images.unsplash.com/photo-1517976487492-5750f3195933?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"/>
   </div>
 </template>
 
@@ -17,6 +21,17 @@ export default {
 </script>
 
 <style>
-#splash {
-}
+/* #splash {
+} */
+
+/* #video-background {
+  position: fixed;
+  right: 0;
+  bottom: 0;
+  min-width: 100%;
+  min-height: 100%;
+  width: auto;
+  height: auto;
+  z-index: -100;
+} */
 </style>
