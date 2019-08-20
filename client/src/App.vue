af360b871440c321210738b93038ccb967f5165f
DANIELA <danielamiritescu@gmail.com>
2019-08-20 11:30:53 +0100

    launches map added to LaunchPopup component

--- client/src/App.vue
+++ client/src/App.vue
@@ -12,6 +12,7 @@ import LaunchTimeline from '@/components/LaunchTimeline.vue';
 import LaunchSplash from '@/components/LaunchSplash.vue';
 import LaunchPopup from '@/components/LaunchPopup.vue';
 
+
 export default {
   name: 'app',
   components: {
--- client/src/components/LaunchPopup.vue
+++ client/src/components/LaunchPopup.vue
@@ -21,6 +21,8 @@
         </v-list-item-content>
       </v-list-item>
 
+      <launch-map v-if="launch.location.pads" :pad="launch.location.pads[0]"/>
+
     <v-img v-if="imageURL" :contain="true" :src="imageURL" height="300"></v-img>
 
       <v-card-text class="desc">
@@ -33,6 +35,7 @@
 
 <script>
 import { eventBus } from '@/main.js';
+import LaunchMap from '@/components/LaunchMap';
 
 export default {
   name: 'LaunchPopup',
@@ -41,6 +44,9 @@ export default {
       launch: null,
       dialog: false
     }
+  },
+  components: {
+    'launch-map': LaunchMap
   },
   computed: {
     imageURL() {
