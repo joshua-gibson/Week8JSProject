f0ae9e33b1413b7cbe424521dc04ed6bd793aa5f
Josh Gibson <flash50k@gmail.com>
2019-08-20 12:58:32 +0100

    works

--- server/db/bulkSubmit.js
+++ server/db/bulkSubmit.js
@@ -7,31 +7,49 @@
 const fetch = require("node-fetch");
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/";
+let completeLaunchData = [];
 
 
+urls = [];
+for (i = 0; i < 170; i++) {
+  urls.push(`https://launchlibrary.net/1.4/launch?sort=%27ascending%27&offset=${i}0`);
+}
 
-let cohort = [];
-let getCohort=function(num){fetch(`https://launchlibrary.net/1.4/launch?sort=%27ascending%27&offset=${num}0`)
-  .then(res=>res.json())
-  .then(res=> cohort = res)
-  .then(cohort=> {
-
+const promises = urls.map(url => fetch(url));
+Promise.all(promises)
+  .then(responses => Promise.all(responses.map(res => res.json())))
+  .then(cohorts => {
+    const mappedCohorts = cohorts.map((cohort) => {
+      return cohort.launches.map((launch) => {
+        const url = `https://launchlibrary.net/1.4/launch/${launch.id}`;
+        return fetch(url);
+      })
+    })
+    return mappedCohorts.flat();
+  })
+  .then((promises) => {
+    return Promise.all(promises);
+  })
+  .then((responses) => {
+    return responses.map((res) => {
+      return res.json()
+      .then(data => {
+        completeLaunchData.push(data.launches);
+        completeLaunchData = completeLaunchData.flat();
+        // console.log(completeLaunchData);
+      })
+    })
+  })
+  .then((res)=>{
     MongoClient.connect(url, function(err, db) {
       if (err) throw err;
       var dbo = db.db("mydb");
-      dbo.collection("launches").insertMany(cohort.launches, function(err, res) {
+      dbo.collection("launches").insertMany(completeLaunchData, function(err, res) {
         if (err) throw err;
         console.log("Number of documents inserted: " + res.insertedCount);
         db.close();
       })
     })
-
-
-
   })
-};
 
-let i=0;
-for (i = 0; i < 170; i++) {
-  getCohort(i);
-}
+  .catch(err => console.log(err));
