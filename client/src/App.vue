1f41b7b5cad7eee20b3fbef7e770bb14325b81d1
lawrie-sm <alertrelic@gmail.com>
2019-08-21 14:25:28 +0100

    Made timer computed:

--- client/src/App.vue
+++ client/src/App.vue
@@ -22,7 +22,4 @@ export default {
 </script>
 
 <style>
-
-
-
 </style>
--- client/src/components/LaunchCountdown.vue
+++ client/src/components/LaunchCountdown.vue
@@ -16,24 +16,25 @@ export default {
     }));
     window.setInterval(() => {
       this.nowTime = new Date().getTime();
-      this.timeTilLaunch = this.launchTime - this.nowTime;
-      const timerDate = new Date(this.timeTilLaunch);
-      const seconds = timerDate.getSeconds();
-      const minutes = timerDate.getMinutes();
-      const hours =  timerDate.getHours();
-      const days = timerDate.getDay();
+      const seconds = this.timerDate.getSeconds();
+      const minutes = this.timerDate.getMinutes();
+      const hours =  this.timerDate.getHours();
+      const days = this.timerDate.getDay();
       this.launchText = `${days} Days ${hours} Hours, ${minutes} Mins, ${seconds} Secs`;
     },1000);
   },
   data() {
     return {
-      now: Math.trunc((new Date()).getTime() / 1000),
-      date: null,
       launchTime: 0,
       nowTime: 0,
-      timeTilLaunch: 0,
       launchText: ''
     }
+  },
+  computed: {
+    timerDate() {
+      const timeLeft = this.launchTime - this.nowTime;
+      return new Date(timeLeft);
+    },
   }
 }
 </script>
